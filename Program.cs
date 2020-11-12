1192441247d89dbbb73bb29ceb261ee6199fdcd0
NtaiBanik <NitaiBanik29@gmail.com>
2020-11-12 13:25:44 +0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Security.Cryptography;
+
+namespace CheckBase64
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var RNG = new RNGCryptoServiceProvider();
+            var bytes = new byte[32];
+            RNG.GetBytes(bytes);
+            var Base64String = Convert.ToBase64String(bytes);
+           
+        }
+    }
+}
